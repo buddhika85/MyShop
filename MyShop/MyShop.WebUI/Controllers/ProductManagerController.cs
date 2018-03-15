a1a6995539a3d9eb952b1901e9356e3a1228a613
buddhika85 <buddhika2006system@gmail.com>
2018-03-15 11:54:06 +0530

    Product Images

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -7,6 +7,7 @@ using MyShop.Core.Models;
 using MyShop.DataAccess.InMemory;
 using MyShop.Core.ViewModels;
 using MyShop.Core.Contracts;
+using System.IO;
 
 namespace MyShop.WebUI.Controllers
 {
@@ -39,18 +40,23 @@ namespace MyShop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(ProductManagerViewModel productToAdd)
+        public ActionResult Create(ProductManagerViewModel productToAdd, HttpPostedFileBase file)
         {
             try
             {
                 if (ModelState.IsValid)
                 {
+                    if (file != null)
+                    {
+                        productToAdd.Product.Image = productToAdd.Product.Id + Path.GetExtension(file.FileName);
+                        file.SaveAs(Server.MapPath("//Content//ProductImages//") + productToAdd.Product.Image);
+                    }
                     context.Insert(productToAdd.Product);
                     context.Commit();
                     return RedirectToAction("Index");
                 }
                 else
-                {
+                {                    
                     return View(productToAdd);
                 }
             }
@@ -78,13 +84,18 @@ namespace MyShop.WebUI.Controllers
             }
         }
 
-        public ActionResult Edit(string id, ProductManagerViewModel productEdited)
+        public ActionResult Edit(string id, ProductManagerViewModel productEdited, HttpPostedFileBase file)
         {
             var product = context.Find(id);
             if (product != null)
             { 
                 if (ModelState.IsValid)
                 {
+                    if (file != null)
+                    {
+                        productEdited.Product.Image = productEdited.Product.Id + Path.GetExtension(file.FileName);
+                        file.SaveAs(Server.MapPath("//Content//ProductImages//") + productEdited.Product.Image);
+                    }
                     product.Name = productEdited.Product.Name;
                     product.Price = productEdited.Product.Price;
                     product.Image = productEdited.Product.Image;
